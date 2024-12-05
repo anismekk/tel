8614b7855643e711ab562547ad3cc926e6b03a3d
anismekk <anis.lakamoura321@gmail.com>
2024-12-05 08:47:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function completeTask(taskId) {
+    alert(`Task ${taskId} completed! You earned 1 Whisker.`);
+    // You can replace this alert with an API call to your backend.
+}
+
+function viewLeaderboard() {
+    alert("Leaderboard coming soon!");
+    // This can later be replaced with a dynamic leaderboard.
+}
